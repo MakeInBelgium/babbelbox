9045c7d6697342c032e73cbd6d404c44f1ca99df
Toon Van de Putte <toon@automaton.be>
2020-03-15 19:13:08 +0100

    detect browser & webcam/mic + verwijzing naar praatbox.be in chat header

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	$("#alert_js").addClass("hidden");
+	$("#roomdata").removeClass("hidden");
+	let alerts = [];
+
 	if (
 		location.protocol !== "https:" &&
 		location.hostname !== "localhost" &&
@@ -9,11 +13,13 @@ $(document).ready(function () {
 			`https:${location.href.substring(location.protocol.length)}`
 		);
 	}
+
 	const domain = "meet.jit.si";
 	var room = getParameterByName("kamer");
 	if (room) {
+		$("body").addClass("fullscreen");
 		$("#chan").removeClass("hidden");
-		$("#roomdata").hide(500);
+		$("#roomdata").addClass("hidden");
 		const options = {
 			roomName: room,
 			parentNode: document.getElementById("meet")
@@ -25,11 +31,73 @@ $(document).ready(function () {
 
 		var api = new JitsiMeetExternalAPI(domain, options);
 	} else {
-		$('#c>option:eq(3)').attr('selected', true);
+		rnd_id = Math.floor($('#c option').length * Math.random());
+		$('#c>option:eq(' + rnd_id + ')').attr('selected', true);
 	}
 
 	$('[data-toggle="tooltip"]').tooltip();
+
+
+	// detect whether user has webcam and microphone
+
+	DetectRTC.load(function () {
+
+		if (false === DetectRTC.isWebRTCSupported) {
+			alerts.push('Je browswer ondersteunt videobellen niet. Je gebruikt best Chrome voor Praatbox.');
+		} else {
+			if ('chrome' === detectBrowser()) {
+				if (0 === DetectRTC.videoInputDevices.length) {
+					alerts.push('Praatbox kan geen camera (webcam) vinden. Je hebt een camera nodig om Praatbox te gebruiken.');
+				}
+
+				if (0 === DetectRTC.audioInputDevices.length) {
+					alerts.push('Praatbox kan geen microfoon vinden. Je hebt een microfoon nodig om Praatbox te gebruiken.');
+				}
+			} else {
+				alerts.push('Je gebruikt een andere webbrowser dan Chrome. Praatbox werkt het best in Chrome.');
+			}
+		}
+
+	});
+
+	let alerttext = '<p>Je praatbox werkt mogelijk niet goed:</p>';
+
+	if (alerts.length > 0) {
+		alerttext += '<ul>';
+		alerts.forEach(function (a) {
+			alerttext += '<li>' + a + '</li>';
+		});
+		alerttext += '</ul>';
+		$("#alert_rtc .alert").html(alerttext);
+		$("#alert_rtc").removeClass("hidden");
+	}
+
 });
+
+function detectBrowser() {
+	//Check if browser is IE
+	if (navigator.userAgent.search("MSIE") >= 0) {
+		return 'msie';
+	}
+	//Check if browser is Chrome
+	else if (navigator.userAgent.search("Chrome") >= 0) {
+		return 'chrome';
+	}
+	//Check if browser is Firefox 
+	else if (navigator.userAgent.search("Firefox") >= 0) {
+		return 'firefox';
+	}
+	//Check if browser is Safari
+	else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+		return 'safari';
+	}
+	//Check if browser is Opera
+	else if (navigator.userAgent.search("Opera") >= 0) {
+		return 'opera';
+	}
+
+	return 'unknown';
+}
 
 function getParameterByName(name, url) {
 	if (!url) url = window.location.href;
@@ -55,7 +123,7 @@ function redirectToRoom() {
 		window.location = "?kamer=" + roomName + "&wrd_a=" + removeCharacters(firstInputField) + "&wrd_b=" + removeCharacters(secondInputField) + "&wrd_c=" + removeCharacters(thirdInputField);
 
 		$("#chan").removeClass("hidden");
-		$("#roomdata").hide(500);
+		$("#roomdata").addClass("hidden");
 	}
 
 	function generateName(firstInputField, secondInputField, thirdInputField) {
