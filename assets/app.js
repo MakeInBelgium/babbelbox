46757a0a3a55a9907fedd2eca83ff2c8be1357dc
Berend Wouters <berend.wouters@gmail.com>
2020-03-15 13:04:26 +0100

    Redirect to channelname on creation

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,40 @@
  $(document).ready(function() {
 	if (location.protocol !== 'https:' && location.hostname !== "localhost" && location.hostname !== "127.0.0.1" && location.hostname !== "0.0.0.0") {
 		location.replace(`https:${location.href.substring(location.protocol.length)}`);
-	}
-	$('[data-toggle="tooltip"]').tooltip();
-});
-
-
-function GenerateRoom() {
-
-	const domain = 'meet.jit.si';
-	var a = document.getElementById("a").value;
-	var b = document.getElementById("b").value;
-	var c = document.getElementById("c").value;
-
-	if( a.value != '' && b.value != '' ) {
+	 }
+	 const domain = 'meet.jit.si';
+	var room = getParameterByName("kamer");
+	 if (room) {
+		 $(".roomdata").hide(500);
 		const options = {
-			roomName: a + b + c ,
+			roomName: room,
 			parentNode: document.getElementById("meet")
 		};
 
 		var api = new JitsiMeetExternalAPI(domain, options);
+	}
 
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return "";
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function redirectToRoom() {
+
+	var firstInputField = document.getElementById("a").value;
+	var secondInputField = document.getElementById("b").value;
+	var thirdInputField = document.getElementById("c").value;
+
+	if( firstInputField !== null && secondInputField !== null && thirdInputField!== null ) {
+		window.location = "?kamer=" + firstInputField + secondInputField + thirdInputField;
 		$(".roomdata").hide(500);
 	}
 
