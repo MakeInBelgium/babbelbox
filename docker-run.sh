9464d91cd228f6de74409dbf52d11944342f9688
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2020-03-17 09:22:29 +0100

    Add support for www & www-less for master

--- docker-run.sh
+++ docker-run.sh
@@ -15,19 +15,36 @@ fi
 
 FULL_DOMAIN="${SUBDOMAIN}.${DOMAIN_NAME}"
 
-docker build -t babbelbox --build-arg vcsref=$(git rev-parse --short HEAD) --build-arg DOMAIN=$FULL_DOMAIN .
+docker build -t babbelbox:$BRANCH --build-arg vcsref=$(git rev-parse --short HEAD) --build-arg DOMAIN=$FULL_DOMAIN .
 docker stop $FULL_DOMAIN && docker rm $FULL_DOMAIN
 
-docker run -d \
-    --network proxy \
-    -l traefik.enable=true \
-    -l traefik.http.routers.bb-$SUBDOMAIN.entrypoints=http \
-    -l traefik.http.routers.bb-$SUBDOMAIN.rule="Host(\`${FULL_DOMAIN}\`)" \
-    -l traefik.http.routers.bb-$SUBDOMAIN.middlewares=https-redirect@file \
-    -l traefik.http.routers.bb-$SUBDOMAIN-secure.entrypoints=https \
-    -l traefik.http.routers.bb-$SUBDOMAIN-secure.rule="Host(\`${FULL_DOMAIN}\`)" \
-    -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls=true \
-    -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls.certresolver=http \
-    -l traefik.docker.network=proxy \
-    --name "${FULL_DOMAIN}" \
-    babbelbox:latest
+if [ $BRANCH == master ]
+then
+    docker run -d \
+        --network proxy \
+        -l traefik.enable=true \
+        -l traefik.http.routers.bb-$SUBDOMAIN.entrypoints=http \
+        -l traefik.http.routers.bb-$SUBDOMAIN.rule="Host(\`${DOMAIN_NAME}\`, \`www.${DOMAIN_NAME}\`)" \
+        -l traefik.http.routers.bb-$SUBDOMAIN.middlewares=https-redirect@file \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.entrypoints=https \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.rule="Host(\`${DOMAIN_NAME}\`, \`www.${DOMAIN_NAME}\`)" \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls=true \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls.certresolver=http \
+        -l traefik.docker.network=proxy \
+        --name "${FULL_DOMAIN}" \
+        babbelbox:$BRANCH
+else
+    docker run -d \
+        --network proxy \
+        -l traefik.enable=true \
+        -l traefik.http.routers.bb-$SUBDOMAIN.entrypoints=http \
+        -l traefik.http.routers.bb-$SUBDOMAIN.rule="Host(\`${FULL_DOMAIN}\`)" \
+        -l traefik.http.routers.bb-$SUBDOMAIN.middlewares=https-redirect@file \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.entrypoints=https \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.rule="Host(\`${FULL_DOMAIN}\`)" \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls=true \
+        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls.certresolver=http \
+        -l traefik.docker.network=proxy \
+        --name "${FULL_DOMAIN}" \
+        babbelbox:$BRANCH
+fi
